711f3884a254abcafe3a65eb8ff5b36fb7ea5726
wrmarte <61038019+wrmarte@users.noreply.github.com>
2025-05-09 18:20:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ client.on('messageCreate', async message => {
     const fakeWallet = '0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF';
     const fakeQty = Math.floor(Math.random() * 5) + 1;
     const fakeEth = (fakeQty * mintPrice).toFixed(4);
-    const testMsg = `>  Embed color: \`#FF00FF\`  🧪 **__TEST MINT TRIGGED (Base Sim)__**\n >  📇 Wallet: \`${fakeWallet}\`\n >  🪶 Quantity: **${fakeQty}**\n >  💰 ETH Spent: **${fakeEth} ETH**`;
+    const testMsg = `>  `  🧪 **__TEST MINT TRIGGED (Base Sim)__**\n >  📇 Wallet: \`${fakeWallet}\`\n >  🪶 Quantity: **${fakeQty}**\n >  💰 ETH Spent: **${fakeEth} ETH**` `;
 
     const channel = await client.channels.fetch(channelId);
     await channel.send(testMsg);
